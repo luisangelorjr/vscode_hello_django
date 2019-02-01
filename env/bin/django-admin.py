ec4217666adfc1e046259da678f50a566cc38053
Luís Ângelo Rodrigues Jr <luisangelorjr@gmail.com>
2019-02-01 18:42:52 -0200

    'explore-the-debugger' is finish
    The next is go-to-definition-and-peek-definition-commands in 'https://code.visualstudio.com/docs/python/tutorial-django#_go-to-definition-and-peek-definition-commands'

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/luisangelorjr/Documentos/DEV/projects/vscode_hello_django/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
